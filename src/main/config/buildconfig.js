16fca54e1ac898777c32073a21016f6e14ae0ee2
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-10-11 20:05:30 +0200

    link to example.build.js in build config

--- src/main/config/buildconfig.js
+++ src/main/config/buildconfig.js
@@ -1,12 +1,15 @@
 ({
+    // for an explanation of these fields, you should go through
+    // https://github.com/jrburke/r.js/blob/master/build/example.build.js
+
     baseUrl: '../webapp',
     inlineText: true,
     useStrict: false,
     name: '../scripts/almond',
     include: ['main'],
     insertRequire: ['main'],
-    out: "../webapp/build/app.js",
-    wrap: false, // not needed when it's not a lib
+    out: '../webapp/build/app.js',
+    wrap: false,
     mainConfigFile: '../webapp/main.js',
     preserveLicenseComments: true,
     logLevel: 0,
