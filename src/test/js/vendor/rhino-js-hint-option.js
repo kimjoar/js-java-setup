929f00f66c1ea85e8eb16cf849b0a5eade6ac179
Hans Magnus Inderberg <hinderberg@gmail.com>
2012-10-23 10:39:17 +0200

    Added jshint

---
+++ src/test/js/vendor/rhino-js-hint-option.js
@@ -0,0 +1,25 @@
+var JSHINT_MERCY = 60;
+
+var JSLINT_OPTIONS = {
+
+    // Environments
+
+    browser     : true,     // assume browser global variables (document, window...)
+    jquery      : true,     // assume jQuery global variables
+    es5         : true,     // assume EcmaScript 5 compatibility (we use a shim for old browsers)
+
+    // Options
+
+    curly       : true,     // if curly braces around blocks should be required (even in if/for/while)
+    debug       : false,    // if debugger statements should be allowed
+    eqeqeq      : true,     // if === should be required
+    evil        : false,    // if eval should be allowed
+    forin       : false,    // if for in statements must filter
+    immed       : true,     // if immediate invocations must be wrapped in parens
+    noarg       : true,     // if arguments.caller and arguments.callee should be disallowed
+    noempty     : true,     // if empty blocks should be disallowed
+    onevar      : false,    // if only one var statement per function should be allowed
+    undef       : true,     // if variables should be declared before used
+    predef      : ['define', 'require', 'requirejs']     // pre defined variables, functions, objects
+
+};
---
+++ src/test/js/vendor/rhino-js-hint.js
@@ -0,0 +1,36 @@
+load("jshint.js");
+load("rhino-js-hint-option.js");
+
+(function rhinoJSHint(args) {
+
+    function getErrors(file) {
+
+        var input = readFile(file);
+        if (!input) {
+            print('jshint: Couldn\'t open file ' + file);
+            return 1;
+        }
+
+        if (!JSHINT(input, JSLINT_OPTIONS)) {
+            var shortName = (file.length <= 40) ? file : '...' + file.substring(file.length - 40);
+            print('\nErrors in ' + shortName + '\n');
+            JSHINT.errors.forEach(function (err) {
+                print('    ' + err.reason + ' (line: ' + err.line + ', character: ' + err.character + ')');
+                print('    > ' + (err.evidence || '').replace(/^\s*(\S*(\s+\S+)*)\s*$/, "$1") + '\n');
+            });
+            return JSHINT.errors.length;
+        }
+
+        return 0;
+    }
+
+    var totalErrors = 0;
+    var files = Array.prototype.slice.call(args);
+    var errors = files.forEach(function (file) {
+        totalErrors += getErrors(file);
+    });
+
+    print('JsHint completed with ' + totalErrors + ' errors (mercy is ' + JSHINT_MERCY + ')');
+    quit (totalErrors <= JSHINT_MERCY ? 0 : 1);
+
+}(arguments));
