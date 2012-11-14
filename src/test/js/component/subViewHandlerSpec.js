d306d7cb5d4b51b35c2166b693070302e8b1aadd
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-11-14 16:06:41 +0100

    Add specs for subview handling

---
+++ src/test/js/component/subViewHandlerSpec.js
@@ -0,0 +1,37 @@
+define(['component/subViewHandler', 'sinon'], function(SubViewHandler, sinon) {
+
+    describe('subview handler', function() {
+
+        it('can add and destroy views', function() {
+            var view = {
+                destroy: sinon.spy()
+            };
+
+            var subViewHandler = new SubViewHandler();
+
+            subViewHandler.addSubView(view);
+
+            subViewHandler.destroySubViews();
+
+            expect(view.destroy).toHaveBeenCalledOnce();
+        });
+
+        it('will only add the same view once', function() {
+            var view = {
+                destroy: sinon.spy()
+            };
+
+            var subViewHandler = new SubViewHandler();
+
+            subViewHandler.addSubView(view);
+            subViewHandler.addSubView(view);
+            subViewHandler.addSubView(view);
+
+            subViewHandler.destroySubViews();
+
+            expect(view.destroy).toHaveBeenCalledOnce();
+        });
+
+    });
+
+});
